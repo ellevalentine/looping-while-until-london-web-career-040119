dce4d7ae06b5f7b512840d046ee6f0170e629db9
Elle Valentine <elle.valentine@hotmail.com>
2019-03-21 04:12:03 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,10 +1,12 @@
 def using_until
   levitation_force = 6
  
- while levitation_force == 10
+ while levitation_force < 10
  
- puts 
+ puts "Wingardium Leviosa"
  
+ levitation_force += 1
+
     
 end
 
